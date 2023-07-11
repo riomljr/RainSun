9fa3ff81e2ab8737e50ac7772d9a9b8a6a528c48
riomljr <22552727+riomljr@users.noreply.github.com>
2023-07-11 12:08:49 -0500

    update to https to make secure requests

--- src/fetchweather.js
+++ src/fetchweather.js
@@ -1,7 +1,7 @@
 
 async function getData (location = "New York") {
   const key = 'ae97ffa5fd484ce4991153204232205';
-  const baseUrl = 'http://api.weatherapi.com/v1/current.json?key=';
+  const baseUrl = 'https://api.weatherapi.com/v1/current.json?key=';
   const loc = '&q=' + location.toString() + '&aqi=no';
 
   const fullUrl = baseUrl + key + loc;
