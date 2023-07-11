cd93271cb4be3c4ce1453e58a9ca47e8278293c7
riomljr <22552727+riomljr@users.noreply.github.com>
2023-07-11 13:31:40 -0500

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,18 +16,6 @@ module: {
         { test: /\.(png|svg|jpg|jpeg|gif)$/i,
           type: 'asset/resource',
         },
-        {
-          test: /\.(?:js|mjs|cjs)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-              ]
-            } 
-          }
-        } 
       ],
     },
 
